b1edb937a8cff85243450049425f69043c7960de
Cosmitar <mhherrera31@gmail.com>
2015-10-29 17:36:03 -0300

    exclude duplicated songs in playlist

--- src/core/ComboMaker.js
+++ src/core/ComboMaker.js
@@ -1,5 +1,6 @@
 'use strict'
 import ComboActions from './../actions/ComboActions';
+import CombosListStore from './../stores/CombosListStore';
 
 class Combo {
     constructor(){
@@ -10,29 +11,59 @@ class Combo {
 }
 
 class ComboMaker {
-    makeCombo(artist) {
-        let selectedArtist = artist;
-        let pairTracks;
-        selectedArtist.getTopTracks('US')//@TODO set user country.
-        .then( trackList => {
-            if( trackList.length < 2 ){
-                //save combo searching albums by popularity.
-                console.warn('Artist has less than 2 songs on top-tracks');
-            }
-            //sort tracks by popularity (not always are well sorted).
-            pairTracks = trackList.sort((a,b) => {
-                return b.popularity - a.popularity;
-            }).slice(0,2);//@TODO check if two songs isn't it the same on different versions
-            //@TODO check if track isn't already in the paylist
+    _getCurrentList() {
+        let retVal = new Map();
+        for( let combo of CombosListStore.getList() ){
+            retVal.set( combo.tracks[0].id, combo.tracks[0] );
+            retVal.set( combo.tracks[1].id, combo.tracks[1] );
+        };
+        return retVal;
+    }
+    _getPairByPopularity(trackList) {
+        let currentList = this._getCurrentList();
+        let pair = [];
+        //sort tracks by popularity (not always are well sorted).
+        let sortedTracks = trackList.sort((a,b) => {
+            return b.popularity - a.popularity;
+        });
 
+        for( let track of sortedTracks ){
+            //check if track isn't already in the paylist
+            if( !currentList.has(track.id) ){
+                pair.push( track );
+            }else{
+                console.warn('Track already in list: '+ track.name);
+            }
+            if( pair.length == 2 ){
+                break;
+            }
+        }
+
+        return pair;
+    }
+    
+    makeCombo(artist) {
+        let pairTracks;
+        artist.getTopTracks('US')//@TODO set user country.
+        .then( trackList => {
+            pairTracks = this._getPairByPopularity(trackList);
+            if( pairTracks.length < 2 ){
+                //@TODO build combo by searching albums by popularity.
+                console.warn('less than 2 songs found');
+            }else{
+                this._buildCombo(artist,pairTracks);
+            }
+        });
+    }
+
+    _buildCombo(artist, pairTracks){
             //@TODO analyze the tracks and catalog the combo.
             let combo = new Combo();
-            combo.id = selectedArtist.id + pairTracks[0].id + pairTracks[1].id;
+            combo.id = artist.id + pairTracks[0].id + pairTracks[1].id;
             combo.type = 'TRENDING';//CLASSIC, HIT, DISCOVER
-            combo.artist = selectedArtist;
+            combo.artist = artist;
             combo.tracks = pairTracks;
             ComboActions.addCombo(combo);
-        });
     }
 
     suggestArtist( referenceArtists, amount = 0 ) {
