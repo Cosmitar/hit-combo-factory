b9cc0fb987e1b6167f018bd1fd6a32256f44d54a
Cosmitar <mhherrera31@gmail.com>
2015-11-10 15:33:29 -0300

    add intro text

--- src/components/WaterMark.js
+++ src/components/WaterMark.js
@@ -5,6 +5,13 @@ class WaterMark extends Component {
     render() {
         return (
             <div className={this.props.className + " WaterMark-continer" }>
+                <div className="mainCopy">
+                <h3>How it works</h3>
+                Choose an artist, let me suggest you the others.<br/>
+                You'll get a playlist with 2 Hit Combo for each artist of your choice.<br/>
+                <small>Relax! you can remove an artis or a song if you don't like it.</small>
+                </div>
+
                 <img src="style/images/watermark.png" className="WaterMark-image" />
             </div>
         );
