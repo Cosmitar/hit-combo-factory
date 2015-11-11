895050554b265379b8a9d3c1eaa62e965790d5aa
Cosmitar <mhherrera31@gmail.com>
2015-11-11 15:34:02 -0300

    fix typo

--- src/components/WaterMark.js
+++ src/components/WaterMark.js
@@ -9,7 +9,7 @@ class WaterMark extends Component {
                     <h3>How it works</h3>
                     Choose an artist, let me suggest you the others.<br/>
                     You'll get a playlist with 2 Hit Combo for each artist of your choice.<br/>
-                    <small>Relax! you can remove an artis or a song if you don't like it.</small>
+                    <small>Relax! you can remove an artist or a song if you don't like it.</small>
                     <br/><br/>
                     built with <img src="style/images/spotify-logo.png" width="70" className="spotify-logo"/> Web API
                 </div>
